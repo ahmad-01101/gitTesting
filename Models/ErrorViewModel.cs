02c97f5f0994db6fd7c79dd3298d21e9a87910e1
Ahamd Shenqiti <ahmadshenqiti10@gmail.com>
2024-04-29 09:10:32 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gitTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
